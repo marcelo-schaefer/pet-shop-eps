bac9477740f4ea99c6e7702641d1c10d00afcf0e
Lucas Nunes <lucas.nunes@senior.com.br>
2023-03-21 13:09:41 -0300

    Retirando pacote entities

--- src/br/com/pet_shop/tela/LerTela.java
+++ src/br/com/pet_shop/tela/LerTela.java
@@ -1,87 +1,87 @@
 package br.com.pet_shop.tela;
 
+import br.com.pet_shop.enums.SexoEnum;
+
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Scanner;
 
-import br.com.pet_shop.enums.SexoEnum;
-
 public final class LerTela {
 
-	private static Scanner scanner = null;
+    private static Scanner scanner = null;
 
-	private LerTela() {
-	}
+    private LerTela() {
+    }
 
-	public static String lerString() {
-		iniciarScanner();
-		var value = scanner.next();
-		fecharScanner();
+    public static String lerString() {
+        iniciarScanner();
+        var value = scanner.next();
+        fecharScanner();
 
-		return value;
-	}
+        return value;
+    }
 
-	public static Double lerDouble() {
-		iniciarScanner();
-		var value = scanner.nextDouble();
-		fecharScanner();
+    public static Double lerDouble() {
+        iniciarScanner();
+        var value = scanner.nextDouble();
+        fecharScanner();
 
-		return value;
-	}
+        return value;
+    }
 
-	public static Date lerData() throws Exception {
-		var simpleDateForm = new SimpleDateFormat("dd/MM/yyyy");
-		Date date;
+    public static Date lerData() throws Exception {
+        var simpleDateForm = new SimpleDateFormat("dd/MM/yyyy");
+        Date date;
 
-		iniciarScanner();
+        iniciarScanner();
 
-		try {
-			var input = lerString();
-			date = simpleDateForm.parse(input);
-		} catch (Exception exception) {
-			throw new Exception("A data deve ser no formato dd/MM/aaaa");
-		}
+        try {
+            var input = lerString();
+            date = simpleDateForm.parse(input);
+        } catch (Exception exception) {
+            throw new Exception("A data deve ser no formato dd/MM/aaaa");
+        }
 
-		fecharScanner();
+        fecharScanner();
 
-		return date;
-	}
+        return date;
+    }
 
-	public static SexoEnum lerEnum() throws Exception {
-		SexoEnum enumConvert;
+    public static SexoEnum lerEnum() throws Exception {
+        SexoEnum enumConvert;
 
-		iniciarScanner();
-		try {
-			var input = lerString();
+        iniciarScanner();
+        try {
+            var input = lerString();
 
-			enumConvert = SexoEnum.valueOf(input);
-		} catch (Exception exception) {
-			var opcoes = pegarOpcoesEnum(SexoEnum.values());
+            enumConvert = SexoEnum.valueOf(input);
+        } catch (Exception exception) {
+            var opcoes = pegarOpcoesEnum(SexoEnum.values());
 
-			var errorMessage = String.format("Opções válidas: %s", opcoes);
+            var errorMessage = String.format("Opções válidas: %s", opcoes);
 
-			throw new Exception(errorMessage);
-		}
-		fecharScanner();
+            throw new Exception(errorMessage);
+        }
+        fecharScanner();
 
-		return enumConvert;
-	}
+        return enumConvert;
+    }
 
-	private static void iniciarScanner() {
-		scanner = new Scanner(System.in);
-	}
+    private static void iniciarScanner() {
+        scanner = new Scanner(System.in);
+    }
 
-	private static void fecharScanner() {
-		scanner.close();
-	}
-	
-	private static <T extends Enum<T>> String pegarOpcoesEnum(Enum<T>[] enumeration) {
-		var opcoes = "";
+    private static void fecharScanner() {
+        scanner.close();
+    }
 
-		for (var i = 0; i < SexoEnum.values().length; i++) {
-			opcoes += SexoEnum.values()[i].toString().concat("\n");
-		}
-		
-		return opcoes;
-	}
+    private static <T extends Enum<T>> String pegarOpcoesEnum(Enum<T>[] enumeration) {
+        var opcoes = "";
+
+        for (var i = 0; i < SexoEnum.values().length; i++) {
+            opcoes += SexoEnum.values()[i].toString().concat("\n");
+        }
+
+        return opcoes;
+    }
 }
